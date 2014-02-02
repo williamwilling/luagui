68b4f27c34aafc95df096cb0a39e81b98509b144
William Willing <william@williamwilling.com>
2014-02-02 12:04:42 +0100

    A text box now properly reports its text value when you change it in the gui instead of in code.

--- gui/text_box.lua
+++ gui/text_box.lua
@@ -4,7 +4,19 @@ local TextBox = {}
 local metatable = common.create_metatable(TextBox)
 common.add_position(metatable, 'a textbox')
 common.add_size(metatable, 'a textbox')
-common.add_text(metatable, 'a textbox')
+
+function metatable.get_text(object)
+  return object.wx:GetValue()
+end
+
+function metatable.set_text(object, value)
+  if type(value) ~= 'string' and type(value) ~= 'number' then
+    local message = string.format('The %s of %s must be a string, not a %s.', property_name, object_description, type(value))
+    error(message, 3)
+  end
+  
+  object.wx:SetValue(tostring(value))
+end
 
 function TextBox.create(wx_parent)
   local text_box = {}
