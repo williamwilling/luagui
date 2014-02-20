22d13132ff9a0299da52dfab4551148bd8ee5a8a
William Willing <william@williamwilling.com>
2014-02-20 15:10:32 +0100

    The unit testing framework now reports the location of the failed assertion instead of the location of the failed test.

--- test/unit_test.lua
+++ test/unit_test.lua
@@ -5,19 +5,21 @@ local unit_test = {
 
 function unit_test.assert.equal(expected, actual)
   if expected ~= actual then
-    local message = string.format('expected [%s] but got [%s].', expected, actual)
-    table.insert(unit_test.results, message)
+    local caller_info = debug.getinfo(2)
+    
+    local result = {}
+    result.message = string.format('expected [%s] but got [%s].', expected, actual)
+    result.file = string.sub(caller_info.source, 2)
+    result.line = caller_info.currentline
+    
+    table.insert(unit_test.results, result)
   end
 end
 
 function unit_test.add_test(name, test)
-  local caller_info = debug.getinfo(2)
-  
   table.insert(unit_test.tests, {
     name = name,
     run = test,
-    line = caller_info.currentline,
-    file = string.sub(caller_info.source, 2)
   })
 end
 
@@ -27,7 +29,7 @@ function unit_test.run()
     test.run()
     
     for _, result in ipairs(unit_test.results) do
-      io.write(string.format('%s:%i: %s\n', test.file, test.line, result))
+      io.write(string.format('%s:%i: %s\n', result.file, result.line, result.message))
     end
   end
 end
