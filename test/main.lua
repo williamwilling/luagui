7c21d44fa92e2a8b1f703b6ab765491ca1d54afe
William Willing <william@williamwilling.com>
2014-02-20 15:32:30 +0100

    Moved unit tests for the window to a separate file.

--- test/main.lua
+++ test/main.lua
@@ -1,16 +1,5 @@
 local unit_test = require 'test.unit_test'
-local assert = unit_test.assert
-local test = unit_test.add_test
 
-require 'gui'
-
-test('window has a size', function()
-  local window = gui.create_window()
-  window.width = 500
-  window.height = 200
-  
-  assert.equal(500, window.width)
-  assert.equal(200, window.height)
-end)
+require 'test.window'
 
 unit_test.run()
