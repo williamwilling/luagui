b74c5d83466deef74df1237c611b51542755beb5
William Willing <william@williamwilling.com>
2014-02-20 15:17:22 +0100

    Added output to show how many tests have failed.

--- test/unit_test.lua
+++ test/unit_test.lua
@@ -24,14 +24,27 @@ function unit_test.add_test(name, test)
 end
 
 function unit_test.run()
+  io.write('\nRunning unit tests...\n')
+  
+  local failed_test_count = 0
+  
   for _, test in ipairs(unit_test.tests) do
     unit_test.results = {}
     test.run()
     
     for _, result in ipairs(unit_test.results) do
+      failed_test_count = failed_test_count + 1
       io.write(string.format('%s:%i: %s\n', result.file, result.line, result.message))
     end
   end
+  
+  if failed_test_count > 0 then
+    io.write(string.format('%i tests failed.\n', failed_test_count))
+  else
+    io.write('All tests passed.\n')
+  end
+  
+  io.write('\n')
 end
 
 return unit_test
