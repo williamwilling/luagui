1d2a7108101f59d8b3a9189f23ce92664e0a3b26
William Willing <william@williamwilling.com>
2014-02-21 16:40:38 +0100

    Replaced all code testing a text box with unit tests.

--- test/anchor_a_text_box_to_all_sides.lua
+++
@@ -1,8 +0,0 @@
-require 'gui'
-
-local window = gui.create_window()
-local text_box = window:add_text_box()
-text_box.width = window.width / 2
-text_box.anchor = 'all'
-
-gui.run()
--- test/anchor_a_text_box_to_top_left.lua
+++
@@ -1,9 +0,0 @@
-require 'gui'
-
-local window = gui.create_window()
-local text_box = window:add_text_box()
-text_box.y = window.height / 2
-text_box.width = window.width / 2
-text_box.anchor = 'top left'
-
-gui.run()
