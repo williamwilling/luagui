216ede2628e2015ec5503c6817057327a1ef275d
William Willing <william@williamwilling.com>
2014-05-04 21:21:29 +0200

    You can now specify wildcards for the open file dialog.

--- gui/open_file_dialog.lua
+++ gui/open_file_dialog.lua
@@ -3,6 +3,43 @@ local common = require 'gui.common'
 local OpenFileDialog = {}
 local metatable = common.create_metatable(OpenFileDialog)
 
+local function create_wildcard_string(wildcards)
+  if wildcards == nil then
+    return ''
+  end
+
+  if type(wildcards) ~= 'table' then
+    error('The wildcards-property is not in the correct format.', 3)
+  end
+  
+  local result = ''
+
+  for _, wildcard in ipairs(wildcards) do
+    if type(wildcard) ~= 'table' or #wildcard < 2 then
+      error('The wildcards-property is not in the correct format.', 3)
+    end
+    
+    local description = wildcard[1]
+    local pattern = ''
+    
+    for i = 2, #wildcard do
+      if #pattern > 0 then
+        pattern = pattern .. ';'
+      end
+      
+      pattern = pattern .. wildcard[i]
+    end
+    
+    if #result > 0 then
+      result = result .. '|'
+    end
+    
+    result = result .. description .. '|' .. pattern 
+  end
+  
+  return result
+end
+
 function OpenFileDialog.create(parent)
   local open_file_dialog = {
     parent = parent,
@@ -24,7 +61,9 @@ function OpenFileDialog:show()
     parent_wx = parent.wx
   end
   
-  local wx_dialog = wx.wxFileDialog(parent_wx, '', '', '', '', style)
+  local wildcard = create_wildcard_string(self.wildcards)
+  
+  local wx_dialog = wx.wxFileDialog(parent_wx, '', '', '', wildcard, style)
   
   local result = wx_dialog:ShowModal() == wx.wxID_OK
   self.file_name = wx_dialog:GetPath()
