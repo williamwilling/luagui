178484df1d3adc286eb0d20491f81bf6ab449f3d
William Willing <william@williamwilling.com>
2014-05-04 21:44:41 +0200

    The error message for invalid filters is now displayed when you set the filters instead of when you show the open file dialog.

--- gui/open_file_dialog.lua
+++ gui/open_file_dialog.lua
@@ -3,22 +3,28 @@ local common = require 'gui.common'
 local OpenFileDialog = {}
 local metatable = common.create_metatable(OpenFileDialog)
 
+function metatable.set_filters(object, value)
+  if (type(value) ~= 'table' and value ~= nil) then
+    error('The filters are not specified in the correct format.', 3)
+  end
+  
+  for _, filter in ipairs(value) do
+    if type(filter) ~= 'table' or #filter < 2 then
+      error('The filters are not specified in the correct format.', 3)
+    end
+  end
+  
+  return value
+end
+
 local function create_filter_string(filters)
   if filters == nil then
     return ''
   end
-
-  if type(filters) ~= 'table' then
-    error('The filters are not specified in the correct format.', 3)
-  end
-  
+    
   local result = ''
 
   for _, filter in ipairs(filters) do
-    if type(filter) ~= 'table' or #filter < 2 then
-      error('The filters are not specified in the correct format.', 3)
-    end
-    
     local description = filter[1]
     local pattern = ''
     
