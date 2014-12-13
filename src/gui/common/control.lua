5b741dc6e77231d5d46d9a7167ce01cb7051ab6b
William Willing <william@williamwilling.com>
2014-12-13 12:54:00 +0100

    Control tables are now cleared out when a control is destroyed.

--- src/gui/common/control.lua
+++ src/gui/common/control.lua
@@ -1,5 +1,10 @@
 return function(control)
   control.destroy = function(self)
     self.wx:Destroy()
+    setmetatable(self, nil)
+    
+    for k in pairs(self) do
+      self[k] = nil
+    end
   end
 end
